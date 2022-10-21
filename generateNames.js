f3555819d397404eb1c1fd3e2a3feaeaf82c0702
Quertte <a.injen90@gmail.com>
2022-10-21 19:58:03 +0300

    final task, user enter or getRndomInteger

--- generateNames.js
+++ generateNames.js
@@ -1,6 +1,7 @@
 const randomProfile = require('random-profile-generator');
+const getRandomInteger = require('./getRandomInteger');
 
-module.exports = function generateNames(count = process.argv[2]) {
+module.exports = function generateNames(count = process.argv[2] || getRandomInteger()) {
   const arrNames = [];
   for (let i = 0; i < count; i++) {
     arrNames.push(randomProfile.name());
---
+++ getRandomInteger.js
@@ -0,0 +1,5 @@
+function getRandomInteger() {
+  return Math.floor(Math.random() * (20 - 1 + 1)) + 1;
+}
+
+module.exports = getRandomInteger;
